0ef178cb41653de96a7308503fd65e27365afe91
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-08-05 10:40:51 +0200

    Type conversion
    (cherry picked from commit 342ae9794e7ebc9c95f185e937d3c90127e349a8)

--- src/main/kotlin/ru/otus/homework/MyInt.kt
+++ src/main/kotlin/ru/otus/homework/MyInt.kt
@@ -1,6 +1,8 @@
 package ru.otus.homework
 
-class MyInt(val value: Int) { // value - это свойство класса (property)
+import kotlin.random.Random
+
+class MyInt(val value: Int): Any() { // value - это свойство класса (property)
     // Методы преобразования типов
     fun toDouble(): Double = value.toDouble()
     fun toLong(): Long = value.toLong()
@@ -28,4 +30,18 @@ fun main() {
     println("i = ${i1.value}") // Обращение к свойству value
     val l = i1.toLong()        // Вызов метода toLong()
     val i3 = i1 + i2           // Вызов переопределенного оператора '+'
+
+    // Декларируемый тип - Any, но фактический тип - MyInt
+    val a1: Any = MyInt.MAX_VALUE
+    // Явное приведение типа - на ваш страх и риск
+    val d = (a1 as MyInt).toDouble()
+
+    // Декларируемый тип - Any, но фактический тип может быть разным
+    val a2: Any = if (Random.nextBoolean()) MyInt.MIN_VALUE else 100500L
+    // Метод определен в супертипе
+    val a2s = a2.toString()
+    // Проверяем, является ли переменная экземпляром MyInt
+    if (a2 is MyInt) {
+        a2.toDouble()
+    }
 }
