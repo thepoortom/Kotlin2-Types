05dfa01959d2eb31f7161d9433800cd8c6dc097c
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-08-05 12:15:00 +0200

    Nullable string
    (cherry picked from commit 37ebdeb43c75c66906a221d668a2e753df341de9)

---
+++ src/main/kotlin/ru/otus/homework/Nullability.kt
@@ -0,0 +1,14 @@
+@file:Suppress("KotlinConstantConditions")
+
+package ru.otus.homework
+
+fun main() {
+    val str1: String = "Hello, World!"
+    val str2: String? = null
+
+    // ...
+    // Сильно ниже по коду в строке 100500
+    // ...
+    println(str1.length)
+    println(str2?.length)
+}
