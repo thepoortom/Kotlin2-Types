3b46d83eea641746ae2b62078f4157ad71988239
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-08-05 12:24:35 +0200

    Nullable smartcast
    (cherry picked from commit dcebddd403c123462fc212eb33c538b0fc0958a8)

--- src/main/kotlin/ru/otus/homework/Nullability.kt
+++ src/main/kotlin/ru/otus/homework/Nullability.kt
@@ -1,14 +1,17 @@
-@file:Suppress("KotlinConstantConditions")
+@file:Suppress("KotlinConstantConditions", "RedundantNullableReturnType")
 
 package ru.otus.homework
 
 fun main() {
     val str1: String = "Hello, World!"
-    val str2: String? = null
+    val str2: String? = "Hello, Null!"
 
     // ...
     // Сильно ниже по коду в строке 100500
     // ...
     println(str1.length)
-    println(str2?.length)
+    if (null != str2) {
+        println(str2.length)
+        println(str2.first())
+    }
 }
