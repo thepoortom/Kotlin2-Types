7faabf2d18b46f4acdaee6a599739cb527a3dc98
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2025-10-15 10:02:34 +0200

    Multiple bounds

--- src/main/kotlin/ru/otus/homework/Bounds.kt
+++ src/main/kotlin/ru/otus/homework/Bounds.kt
@@ -2,14 +2,34 @@
 
 package ru.otus.homework
 
+import java.util.stream.IntStream
+
 fun main() {
-    class Box<T : CharSequence?>(private val value: T) {
+    class Box<T>(private val value: T) where T : CharSequence?, T : FirstChar? {
         fun getValue(): T = value
+        fun getLength(): Int = value?.length ?: 0
+        fun getFirstChar(): Char? = value?.firstChar
     }
 
-    val strBox: Box<String> = Box("String 1")
-    val nStrBox: Box<String?> = Box(null)
+    val strBox: Box<MyString> = Box(MyString("String 1"))
+    val nStrBox: Box<MyString?> = Box(null)
 
-    val strValue: String = strBox.getValue()
-    val nStrValue: String? = nStrBox.getValue()
+    val strValue: CharSequence = strBox.getValue()
+    val nStrValue: CharSequence? = nStrBox.getValue()
+}
+
+data class MyString(val value: String): CharSequence by value, FirstChar {
+    override val firstChar: Char get() = value[0]
+
+    override fun chars(): IntStream {
+        return value.chars()
+    }
+
+    override fun codePoints(): IntStream {
+        return value.codePoints()
+    }
+}
+
+private interface FirstChar {
+    val firstChar: Char
 }
